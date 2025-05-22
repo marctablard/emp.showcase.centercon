2ff52399dcf1f97db8b7a915d8aa08e67cdcf80f
Michael Feige <mfeige@medienwerft.de>
2025-05-22 11:38:46 +0200

    Fix for EmporixServerConfig

--- src/platform/integrations/emporix/config/impl/EmporixConfigServer.ts
+++ src/platform/integrations/emporix/config/impl/EmporixConfigServer.ts
@@ -1,14 +1,15 @@
 import { injectable } from "@/platform/core/di/injectable";
-import type { EmporixConfig as IEmporixConfig } from "..";
+import type { EmporixConfig as IEmporixConfig} from "..";
 
 @injectable('EmporixConfig', 'Singleton')
+
 class EmporixConfigServer implements IEmporixConfig {
-  baseUrl: string = process.env.NEXT_EMPORIX_BASE_URL || 'https://api.emporix.io';
-  tenant: string = process.env.NEXT_EMPORIX_TENANT || '';
-  clientId: string = process.env.NEXT_EMPORIX_CLIENT_ID || '';
-  clientSecret: string = process.env.NEXT_EMPORIX_CLIENT_SECRET || '';
-  serverClientId?: string = process.env.NEXT_EMPORIX_SERVER_CLIENT_ID;
-  serverClientSecret?: string = process.env.NEXT_EMPORIX_SERVER_CLIENT_SECRET;
+  baseUrl: string = process.env.NEXT_PUBLIC_EMPORIX_BASE_URL || 'https://api.emporix.io';
+  tenant: string = process.env.NEXT_PUBLIC_EMPORIX_TENANT || '';
+  clientId: string = process.env.NEXT_PUBLIC_EMPORIX_CLIENT_ID || '';
+  clientSecret: string = process.env.NEXT_PUBLIC_EMPORIX_CLIENT_SECRET || '';
+  serverClientId?: string = process.env.NEXT_EMPORIX_CLIENT_ID;
+  serverClientSecret?: string = process.env.NEXT_EMPORIX_CLIENT_SECRET;
 }
 
 export default EmporixConfigServer
