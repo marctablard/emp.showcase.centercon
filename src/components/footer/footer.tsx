200e410c1a209db327ad11f86b7edfee81b785b1
atonke <atonke@medienwerft.de>
2025-06-12 16:29:38 +0200

    SHOW-66 add images

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import Link from 'next/link';
 import { Instagram, Linkedin, Youtube } from 'lucide-react';
 import { cn } from '@/lib/utils';
@@ -9,27 +10,45 @@ export default function Footer() {
   const t = useTranslations('footer');
   return (
     <footer className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-6 p-4 lg:p-6">
-        <div className="text-sm bg-primary-50 rounded-tl-2xl p-2 md:p-4">
-          <div className="font-bold">{t('companyName')}</div>
-          <div>{t('street')}</div>
-          <div>{t('city')}</div>
-          <div>{t('country')}</div>
+      <div className="text-sm grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-6 p-4 lg:p-6">
+        <div>
+          <p className="font-bold mb-2">Join Our Newsletter for Exclusive Updates and More!</p>
+          <p>I have acknowledged the privacy policy and read the terms and conditions, and I agree with them.</p>
         </div>
-        <div className="text-sm bg-primary-50 rounded-br-2xl p-2 md:p-4 flex flex-col gap-2">
-          <div>
-            <div className="font-bold">Our service hours</div>
-            <div>Monday to Thursday: 8:00 AM to 5:00 PM</div>
-            <div>Friday: 8:00 AM to 2:00 PM</div>
+        <div>
+          <p className="font-bold mb-2 md:mb-6">Every purchase easily paid and secured.</p>
+          <div className="flex gap-2 justify-between items-center max-w-[400px]">
+            <Image src="mastercard.svg" alt="Mastercard" width="88" height="20" />
+            <Image src="visa.svg" alt="Visa" width="32" height="10" />
+            <Image src="paypal.svg" alt="Paypal" width="60" height="16" />
+            <Image src="pci-dss-compliant.svg" alt="PCI DSS Compliant" width="50" height="20" />
+            <Image src="gdpr.svg" alt="GDPR" width="19" height="24" />
           </div>
-          <div className="grid grid-cols-2 gap-2">
+        </div>
+        <div className="bg-primary-50 rounded-tl-2xl p-2 md:p-4">
+          <div className="bg-[url('/map-pinned.svg')] bg-no-repeat bg-right-top">
+            <p className="font-bold">{t('companyName')}</p>
+            <p>{t('street')}</p>
+            <p>{t('city')}</p>
+            <p>{t('country')}</p>
+          </div>
+        </div>
+        <div className="bg-primary-50 rounded-br-2xl p-2 md:p-4">
+          <div className="bg-[url('/headset.svg')] bg-no-repeat bg-right-top flex flex-col gap-2">
             <div>
-              <div className="font-bold">Service & Technology</div>
-              <div>0123 987654-32</div>
+              <p className="font-bold">Our service hours</p>
+              <p>Monday to Thursday: 8:00 AM to 5:00 PM</p>
+              <p>Friday: 8:00 AM to 2:00 PM</p>
             </div>
-            <div>
-              <div className="font-bold">Sales</div>
-              <div>0123 987654-31</div>
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <p className="font-bold">Service & Technology</p>
+                <p>0123 987654-32</p>
+              </div>
+              <div>
+                <p className="font-bold">Sales</p>
+                <p>0123 987654-31</p>
+              </div>
             </div>
           </div>
         </div>
@@ -165,9 +184,9 @@ export function LegalFooter() {
       data-slot="legal-footer"
       className="max-w-6xl w-full mx-auto px-4 pb-16 pt-2 md:px-8 md:py-0 md:h-8 bg-primary-500 md:rounded-full"
     >
-      <div className="flex gap-2 lg:gap-6 flex-wrap text-sm/8 text-white">
-        <p className="flex-grow">© 2025 Emporix. All Rights Reserved.</p>
-        <div className="flex gap-6">
+      <div className="flex flex-col md:flex-row gap-2 lg:gap-6 flex-wrap text-sm/8 text-white">
+        <p className="flex-grow text-center md:text-left">© 2025 Emporix. All Rights Reserved.</p>
+        <div className="flex flex-grow gap-6 justify-between md:justify-end">
           <Link
             href="#"
             className="hover:underline outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white"
