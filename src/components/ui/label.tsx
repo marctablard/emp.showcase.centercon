dc4d7ad2ae3bd835e9e7a3ad714575302242ad8e
skaschper-mw <134293336+skaschper-mw@users.noreply.github.com>
2025-06-12 13:52:45 +0200

    SHOW-64
    Digital Styleguide Page with first iteration of componenents

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,18 +2,38 @@
 
 import * as React from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
+import { Info } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Tooltip, TooltipContent, TooltipTrigger } from './tooltip';
 
-function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+export interface LabelProps extends React.ComponentProps<typeof LabelPrimitive.Root> {
+  isOptional?: boolean;
+  hasTooltip?: boolean;
+  tooltipText?: string;
+}
+function Label({ className, isOptional, hasTooltip, tooltipText, ...props }: LabelProps) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        'flex items-center gap-2 text-base leading-none font-semibold select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
         className,
       )}
       {...props}
-    />
+    >
+      <div>{props.children}</div>
+      {hasTooltip && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Info className="text-primary-500" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="w-full">{tooltipText}</p>
+          </TooltipContent>
+        </Tooltip>
+      )}
+      {isOptional && <p className="text-xs font-normal text-gray-500">(optional)</p>}
+    </LabelPrimitive.Root>
   );
 }
 
