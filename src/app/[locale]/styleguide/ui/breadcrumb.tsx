de0599fe851a69e893d89e762f5f26114721d202
Michael Feige <mfeige@medienwerft.de>
2025-06-13 14:58:55 +0200

    Breadcrumb Molecule for unified logic

--- src/app/[locale]/styleguide/ui/breadcrumb.tsx
+++ src/app/[locale]/styleguide/ui/breadcrumb.tsx
@@ -1,39 +1,30 @@
-import {
-  Breadcrumb,
-  BreadcrumbBackLink,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from '@/components/ui/breadcrumb';
+import { UiBreadcrumb } from '@/components/ui/molecules/UiBreadcrumb';
+import { BreadcrumbConent } from '@/lib/breadcrumb';
 
 export default function BreadcrumbStyleGuide() {
+  const breadcrumbs: BreadcrumbConent[] = [
+    {
+      href: '#',
+      label: 'Link Name',
+    },
+    {
+      href: '#',
+      label: 'Longer Link Name',
+    },
+    {
+      href: '#',
+      label: 'Another Link Name',
+    },
+    {
+      href: '#',
+      label: 'Current Page',
+    },
+  ];
   return (
     <div className="py-12">
       <h4 className="text-3xl/5 lg:text-4xl font-bold text-headlines font-headlines mb-3">Breadcrumb</h4>
-      <div className="py-4 grid grid-cols-[1fr] gap-6 mb-2">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbBackLink href="/" />
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/">This is Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Link Name</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Longer Link Name</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Another Link Name</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbPage>Current Page</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+      <div className="py-4 grid  mb-2">
+        <UiBreadcrumb items={breadcrumbs} />
       </div>
     </div>
   );
