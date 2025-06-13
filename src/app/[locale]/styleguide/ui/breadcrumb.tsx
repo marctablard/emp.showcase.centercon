b9ea0426a0f127366a3d0ee90eda15b0f0a3f0c2
Michael Feige <mfeige@medienwerft.de>
2025-06-13 15:10:58 +0200

    UI Add Padding to Molecule

--- src/app/[locale]/styleguide/ui/breadcrumb.tsx
+++ src/app/[locale]/styleguide/ui/breadcrumb.tsx
@@ -23,7 +23,7 @@ export default function BreadcrumbStyleGuide() {
   return (
     <div className="py-12">
       <h4 className="text-3xl/5 lg:text-4xl font-bold text-headlines font-headlines mb-3">Breadcrumb</h4>
-      <div className="py-4 grid  mb-2">
+      <div className="grid  mb-2">
         <UiBreadcrumb items={breadcrumbs} />
       </div>
     </div>
--- src/components/ui/molecules/ui-breadcrumb.tsx
+++ src/components/ui/molecules/ui-breadcrumb.tsx
@@ -40,7 +40,7 @@ export function UiBreadcrumb({ items, maxItems = 2, className, ...props }: UiBre
   const hiddenItems = items.slice(0, items.length - effectiveMaxItems);
 
   return (
-    <Breadcrumb className={cn('w-full', className)} {...props}>
+    <Breadcrumb className={cn('w-full py-4', className)} {...props}>
       <BreadcrumbList>
         {/* Back button - always present */}
         <BreadcrumbItem>
