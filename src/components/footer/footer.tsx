32d2319b5645e7e446b0ee051015584e18510954
atonke <atonke@medienwerft.de>
2025-06-13 11:16:33 +0200

    add translations

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
     <footer className="w-full">
       <div className="text-sm grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-6 p-4 lg:p-6">
         <div>
-          <p className="font-bold mb-2">Join Our Newsletter for Exclusive Updates and More!</p>
+          <p className="font-bold mb-2">{t('newsletterTitle')}</p>
           <div className="max-w-[560px]">
             <Form {...newsletterForm}>
               <form onSubmit={newsletterForm.handleSubmit(newsletterForm.onSubmit)}>
@@ -28,7 +28,7 @@ export default function Footer() {
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <InputButton placeholder="Email address" {...field} iconButtonAfter={Mail} />
+                        <InputButton placeholder={t('newsletterInput')} {...field} iconButtonAfter={Mail} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -38,19 +38,19 @@ export default function Footer() {
             </Form>
           </div>
           <p className="mt-2">
-            I have acknowledged the{' '}
+            {t('beforePrivacyPolicy')}
             <UiLink type="Link" variant="text" href="/privacy-policy">
-              privacy policy
-            </UiLink>{' '}
-            and read the{' '}
-            <UiLink type="Link" variant="text" href="/terms-and-conditions">
-              terms and conditions
+              {t('privacyPolicyLink')}
             </UiLink>
-            , and I agree with them.
+            {t('beforeTermsAndConditions')}
+            <UiLink type="Link" variant="text" href="/terms-and-conditions">
+              {t('termsAndConditionsLink')}
+            </UiLink>
+            {t('afterTermsAndConditions')}
           </p>
         </div>
         <div>
-          <p className="font-bold mb-2 md:mb-6">Every purchase easily paid and secured.</p>
+          <p className="font-bold mb-2 md:mb-6">{t('paymentMethodsTitle')}</p>
           <div className="flex gap-2 justify-between items-center max-w-[400px]">
             <Image src="/images/mastercard.svg" alt="Mastercard" width="88" height="20" />
             <Image src="/images/visa.svg" alt="Visa" width="32" height="10" />
@@ -70,17 +70,17 @@ export default function Footer() {
         <div className="bg-primary-50 rounded-br-2xl p-2 md:p-4">
           <div className="bg-[url('/headset.svg')] bg-no-repeat bg-right-top flex flex-col gap-2">
             <div>
-              <p className="font-bold">Our service hours</p>
-              <p>Monday to Thursday: 8:00 AM to 5:00 PM</p>
-              <p>Friday: 8:00 AM to 2:00 PM</p>
+              <p className="font-bold">{t('ourServiceHours')}</p>
+              <p>{t('mondayToThursday')}</p>
+              <p>{t('friday')}</p>
             </div>
             <div className="grid grid-cols-2 gap-2">
               <div>
-                <p className="font-bold">Service & Technology</p>
+                <p className="font-bold">{t('serviceTechnology')}</p>
                 <p>0123 987654-32</p>
               </div>
               <div>
-                <p className="font-bold">Sales</p>
+                <p className="font-bold">{t('sales')}</p>
                 <p>0123 987654-31</p>
               </div>
             </div>
@@ -92,56 +92,57 @@ export default function Footer() {
 }
 
 export function FooterLinks() {
-  // this content will be customizable from storyblok later so no frontend translations are needed
+  const t = useTranslations('footerLinks');
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-5 gap-4 lg:gap-6 ml-4 mr-4 lg:ml-6 lg:mr-6 py-4 lg:py-6 border-b border-b-neutral-200">
       <div className="flex flex-col gap-1">
-        <p className="text-xl mb-1">Products</p>
+        <p className="text-xl mb-1">{t('products')}</p>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Solar Panels
+          {t('solarPanels')}
         </UiLink>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Inverters
+          {t('inverters')}
         </UiLink>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Battery Solutions
+          {t('batterySolutions')}
         </UiLink>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Wiring Solutions
+          {t('wiringSolutions')}
         </UiLink>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Solar Accessories
-        </UiLink>
-      </div>
-      <div className="flex flex-col gap-1">
-        <p className="text-xl mb-1">Services</p>
-        <UiLink type="Link" href="#" variant="secondary" size="s">
-          Solar Solutions
-        </UiLink>
-        <UiLink type="Link" href="#" variant="secondary" size="s">
-          Installations
-        </UiLink>
-        <UiLink type="Link" href="#" variant="secondary" size="s">
-          Renewable Energy
-        </UiLink>
-        <UiLink type="Link" href="#" variant="secondary" size="s">
-          Tech Services
+          {t('solarAccessories')}
         </UiLink>
       </div>
       <div className="flex flex-col gap-1">
-        <p className="text-xl mb-1">Solutions</p>
+        <p className="text-xl mb-1">{t('services')}</p>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Installation planning
+          {t('solarSolutions')}
         </UiLink>
         <UiLink type="Link" href="#" variant="secondary" size="s">
-          Regular maintenance
+          {t('installations')}
+        </UiLink>
+        <UiLink type="Link" href="#" variant="secondary" size="s">
+          {t('renewableEnergy')}
+        </UiLink>
+        <UiLink type="Link" href="#" variant="secondary" size="s">
+          {t('techServices')}
         </UiLink>
       </div>
       <div className="flex flex-col gap-1">
-        <p className="text-xl mb-1">Online Planner</p>
+        <p className="text-xl mb-1">{t('solutions')}</p>
+        <UiLink type="Link" href="#" variant="secondary" size="s">
+          {t('installationPlanning')}
+        </UiLink>
+        <UiLink type="Link" href="#" variant="secondary" size="s">
+          {t('regularMaintenance')}
+        </UiLink>
       </div>
       <div className="flex flex-col gap-1">
-        <p className="text-xl mb-1">About us</p>
+        <p className="text-xl mb-1">{t('onlinePlanner')}</p>
+      </div>
+      <div className="flex flex-col gap-1">
+        <p className="text-xl mb-1">{t('aboutUs')}</p>
         <UiLink type="Link" href="#" variant="secondary" size="s">
           <Linkedin />
           Linkedin
@@ -170,32 +171,33 @@ export function FooterWrapper({ className, ...props }: React.ComponentProps<'div
 }
 
 export function LegalFooter() {
-  // this content will be customizable from storyblok later so no frontend translations are needed
+  const t = useTranslations('footerLegal');
+
   return (
     <div
       data-slot="legal-footer"
       className="max-w-6xl w-full mx-auto px-4 pb-16 pt-2 md:px-8 md:py-0 md:h-8 bg-primary-500 md:rounded-full"
     >
       <div className="flex flex-col md:flex-row gap-2 lg:gap-6 flex-wrap text-sm/8 text-white">
-        <p className="flex-grow text-center md:text-left">© 2025 Emporix. All Rights Reserved.</p>
+        <p className="flex-grow text-center md:text-left">{t('copyright')}</p>
         <div className="flex flex-grow gap-6 justify-between md:justify-end">
           <Link
-            href="#"
+            href="/privacy-policy"
             className="hover:underline outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white"
           >
-            Privacy Policy
+            {t('privacyPolicy')}
           </Link>
           <Link
-            href="#"
+            href="/imprint"
             className="hover:underline outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white"
           >
-            Imprint
+            {t('imprint')}
           </Link>
           <Link
-            href="#"
+            href="/terms-and-conditions"
             className="hover:underline outline-none focus-visible:rounded-xs focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white"
           >
-            Terms and Conditions
+            {t('termsAndConditions')}
           </Link>
         </div>
       </div>
