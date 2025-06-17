771beaf2f3050868d6eb0a2085669aaf280235e8
Felix Binder <fbinder@medienwerft.de>
2025-06-17 11:06:35 +0200

    SHOW-47: Update `HeaderCartButton` to conditionally render badge based on `totalItems` count

--- src/components/header/common/header-cart-button.tsx
+++ src/components/header/common/header-cart-button.tsx
@@ -27,9 +27,11 @@ export default function HeaderCartButton({ initialCart }: HeaderCartButtonProps)
         <Button className="pl-4 pr-1 py-1 gap-4 self-center">
           <span className="text-xl">0,00 €</span>
           <div className="flex items-center w-[43px] h-[35px] relative">
-            <Badge variant="white" className="h-5 min-w-5 rounded-full px-1 tabular-nums absolute top-0 right-0">
-              15
-            </Badge>
+            {totalItems > 0 && (
+              <Badge variant="white" className="h-5 min-w-5 rounded-full px-1 tabular-nums absolute top-0 right-0">
+                {totalItems}
+              </Badge>
+            )}
             <ShoppingCart width="32" height="32" />
           </div>
         </Button>
