de540a473896ad74c5597e02380eed7ce7aa0a18
Sarah Kaschper <skaschper@medienwerft.de>
2025-06-24 16:15:54 +0200

    SHOW-55 Fix scrolling

--- src/components/cart/cart-summary.tsx
+++ src/components/cart/cart-summary.tsx
@@ -33,6 +33,8 @@ export function CartSummary({ cart, isDelivery, loading, leftContent }: CartSumm
     );
     const containerBottom = fixedContainer?.current?.getBoundingClientRect().bottom;
     const windowHeight = window.innerHeight;
+    const windowScroll = window.scrollY;
+    console.log('windowScroll', windowScroll);
     const contentBox = leftContent?.current?.getBoundingClientRect();
     const contentBottom = contentBox?.bottom;
     const contentTop = contentBox?.top;
@@ -47,8 +49,12 @@ export function CartSummary({ cart, isDelivery, loading, leftContent }: CartSumm
         if (containerTop && containerTop <= topPosition) {
           setIsFixed(true);
           setIsFixedToTop(true);
-        } else {
           if (contentTop && contentTop > containerTop) {
+            setIsFixed(false);
+            setIsContainerBottom(false);
+          }
+        } else {
+          if ((contentTop && contentTop > containerTop) || windowScroll <= 0) {
             setIsFixed(false);
             setIsContainerBottom(false);
           }
