782ff6b822ac310c892e89ed4073157fca159c5a
sduvvuri1401 <sduvvuri@medienwerft.de>
2025-06-27 15:35:40 +0200

    SHOW-52 : Send specifications in formatted fashion to FE.

--- src/platform/services/model/product/impl/BatteryIncludedProductMapper.ts
+++ src/platform/services/model/product/impl/BatteryIncludedProductMapper.ts
@@ -2,7 +2,11 @@
 import { inject } from 'inversify';
 import { injectable } from '@/platform/core/di/injectable';
 import { BatteryIncludedProduct } from '@/platform/integrations/batteryincluded/model/product';
-import { Product as ServiceProduct } from '@/platform/services/model/product';
+import {
+  GroupedSpecification,
+  ProductSpecification,
+  Product as ServiceProduct,
+} from '@/platform/services/model/product';
 import { LocalizedString, Price } from '../../common';
 import { CategorySuggestion, SearchSuggestions } from '../../search/SearchSuggestions';
 import { SuggestionsMapper } from '../../search/SuggestionsMapper';
@@ -33,6 +37,7 @@ class BatteryIncludedProductMapper implements ProductMapper<BatteryIncludedProdu
     });
 
     // Map price data if available - using mixins data as requested
+    // TODO clarify, what to do when more pricesare returned
     if (product.prices) {
       productData.price = this.mapPrice(product.prices[0]);
     }
@@ -172,26 +177,6 @@ class BatteryIncludedProductMapper implements ProductMapper<BatteryIncludedProdu
 
     // Create a new object to avoid mutating the input
     const enhancedProduct: Product = { ...product };
-
-    if (mixins.specifications?.specifications) {
-      enhancedProduct.specifications = mixins.specifications.specifications.map((spec: any) => ({
-        key: spec.key,
-        label: spec.label?.reduce((acc: LocalizedString, item: any) => {
-          acc[item.language] = item.value;
-          return acc;
-        }, {} as any),
-        value: spec.value?.reduce((acc: LocalizedString, item: any) => {
-          acc[item.language] = item.value;
-          return acc;
-        }, {} as any),
-        ...(spec.unit && {
-          unit: spec.unit?.reduce((acc: LocalizedString, item: any) => {
-            acc[item.language] = item.value;
-            return acc;
-          }, {} as any),
-        }),
-      }));
-    }
 
     if (mixins.usp?.usp) {
       enhancedProduct.usps = mixins.usp.usp.map((usp: any) => ({
