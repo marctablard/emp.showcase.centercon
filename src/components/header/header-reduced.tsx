4e090740fa6811ffe8cba72d495f93767262d568
Michael Feige <mfeige@medienwerft.de>
2025-06-28 10:56:00 +0200

    Height Fix for Logo

--- src/components/header/header-reduced.tsx
+++ src/components/header/header-reduced.tsx
@@ -10,7 +10,7 @@ export function HeaderReduced() {
       <div className="fixed top-0 left-0 right-0 pt-4 px-8 z-50 max-w-6xl mx-auto">
         <header className="flex bg-white h-18 shadow-xl rounded-2xl border px-6 items-center">
           <Link href="/" className="flex-grow">
-            <Image src={'/images/logo.svg'} alt="Logo" width="160" height="16" className="h-16 justify-middle" />
+            <Image src={'/images/logo.svg'} alt="Logo" width="140" height="16" className="h-16 justify-middle" />
           </Link>
           <HeaderCartButton showSum={true} />
         </header>
