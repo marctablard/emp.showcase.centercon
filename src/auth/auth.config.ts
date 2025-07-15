d1f0b1c842532cf594c9a5d9784095172ad88d86
Michael Feige <mfeige@medienwerft.de>
2025-07-15 12:22:29 +0200

    BUGFIX: Added trustHost flag for auth.js

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from 'next-auth';
 
 export default {
   debug: false,
+  trustHost: true,
   theme: { logo: 'https://authjs.dev/img/logo-sm.png' },
   providers: [],
   pages: {
