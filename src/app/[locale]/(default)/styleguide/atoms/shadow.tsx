e39cde885be2ec3b8714c99c4eebec81ae9d2faa
atonke-mw <158997259+atonke-mw@users.noreply.github.com>
2025-07-21 12:17:37 +0200

    Add shadows (#100)
    * feat: add global shadows
    
    * add shadow 2xl

---
+++ src/app/[locale]/(default)/styleguide/atoms/shadow.tsx
@@ -0,0 +1,16 @@
+export default function ShadowStyleGuide() {
+  return (
+    <div className="py-12">
+      <h4 className="text-3xl/5 lg:text-4xl font-bold text-headlines font-headlines mb-3">Shadows</h4>
+      <div className="p-4 flex flex-wrap gap-8 mb-2">
+        <div className="p-4 shadow-xs">shadow-xs</div>
+        <div className="p-4 shadow-sm">shadow-sm</div>
+        <div className="p-4 shadow-md">shadow-md</div>
+        <div className="p-4 shadow-lg">shadow-lg</div>
+        <div className="p-4 shadow-xl">shadow-xl</div>
+        <div className="p-4 shadow-2xl">shadow-2xl</div>
+        <div className="p-4 shadow-footer">shadow-footer</div>
+      </div>
+    </div>
+  );
+}
