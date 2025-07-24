204c84f1e317eb65fb91b863d3e9acddc168aead
Felix Binder <fbinder@medienwerft.de>
2025-07-24 12:07:20 +0200

    Integrate `TopBannerAnnouncement` component from Storyblok into the header and update `UiLink` to support `target` attribute for enhanced flexibility.

--- src/components/header/expanded/header-top-banner.tsx
+++ src/components/header/expanded/header-top-banner.tsx
@@ -1,14 +1,16 @@
 import { useTranslations } from 'next-intl';
-import { ArrowUpRight } from 'lucide-react';
+import TopBannerAnnouncement from '@/components/cms/top-banner-announcement';
 import { LanguageSwitcher } from '@/components/header/common/header-language-switcher';
-import UiLink from '@/components/ui/link';
 import { Separator } from '@/components/ui/separator';
+import { useBreakpoint } from '@/hooks/useBreakpoint';
 import { Link } from '@/i18n/navigation';
 import { CurrencySwitcher } from '../common/header-currency-switcher';
 import { RegionSwitcher } from '../common/header-region-switcher';
 
 export function HeaderTopBanner() {
   const t = useTranslations('header');
+  const isExtraLargeScreen = useBreakpoint('xl');
+
   return (
     <div className="bg-primary text-white shadow-sm rounded-2xl flex items-center -mx-2 lg:-mx-4 -mt-1 h-8 px-8 lg:px-10">
       <div className="flex justify-between items-center self-stretch w-full">
@@ -23,13 +25,11 @@ export function HeaderTopBanner() {
           </div>
           <CurrencySwitcher />
         </div>
-        {/* Todo: add storyblok component */}
-        <div className="hidden xl:flex justify-center items-center font-bold">
-          Here is space for you top banner&nbsp;
-          <UiLink type="Link" className="text-white hover:text-white" iconAfter={<ArrowUpRight className="w-4 h-4" />}>
-            announcements!
-          </UiLink>
-        </div>
+        {isExtraLargeScreen && (
+          <div className="justify-center items-center font-bold">
+            <TopBannerAnnouncement />
+          </div>
+        )}
         <div className="flex grow basis-0 shrink-0 justify-end items-center gap-6 text-nowrap">
           <Link title={t('blog')} href="/blog">
             {t('blog')}
