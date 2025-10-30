718f53e7938365707aef434cd871fc5a2adac46e
Michael Feige <mfeige@medienwerft.de>
2025-10-30 12:12:40 +0100

    Add Handling for Mixin-L10n-Fields

--- jest.config.js
+++ jest.config.js
@@ -4,43 +4,45 @@ const nextJest = require('next/jest');
 // Providing the path to your Next.js app which will enable loading next.config.js and .env files
 const createJestConfig = nextJest({ dir: './' });
 
-
 const commonJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@platform/(.*)$': '<rootDir>/src/platform/$1'
+    '^@platform/(.*)$': '<rootDir>/src/platform/$1',
   },
   // Exclude e2e tests from Jest runs
   testPathIgnorePatterns: [
     '/node_modules/',
     '/e2e/',
     // excluded, because it just provides a common TokenManager for tests but no own tests
-    'src/platform/integrations/emporix/common/impl/EmporixTokenManager.test.ts'
+    'src/platform/integrations/emporix/common/impl/EmporixTokenManager.test.ts',
   ],
-}
+};
 // Any custom config you want to pass to Jest
 const customJestConfig = {
   preset: 'ts-jest',
-  
+
   projects: [
     {
-      preset: 'ts-jest',    
+      preset: 'ts-jest',
       displayName: 'React Tests',
       testEnvironment: 'jsdom',
       testMatch: ['**/hooks/**/?(*.)+(spec|test).ts?(x)'],
       setupFilesAfterEnv: ['<rootDir>/jest.react.setup.js'],
       transform: {
-        '^.+\.(ts|tsx)$': ['@swc/jest', {
-          jsc: {
-            transform: {
-              react: {
-                runtime: 'automatic'
-              }
-            }
-          }
-        }]
+        '^.+\.(ts|tsx)$': [
+          '@swc/jest',
+          {
+            jsc: {
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                },
+              },
+            },
+          },
+        ],
       },
-      ...commonJestConfig
+      ...commonJestConfig,
     },
     {
       preset: 'ts-jest',
@@ -56,8 +58,24 @@ const customJestConfig = {
           },
         ],
       },
-      ...commonJestConfig
-    }
+      ...commonJestConfig,
+    },
+    {
+      preset: 'ts-jest',
+      displayName: 'Library Tests',
+      testEnvironment: 'node',
+      testMatch: ['**/lib/**/?(*.)+(spec|test).ts?(x)'],
+      setupFilesAfterEnv: ['<rootDir>/jest.platform.setup.js'],
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            tsconfig: 'tsconfig.json',
+          },
+        ],
+      },
+      ...commonJestConfig,
+    },
   ],
 };
 
