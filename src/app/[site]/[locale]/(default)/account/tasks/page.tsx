650acfc0d5486d2701f9b3eae7a92a92fea1b05d
David Gurk <dgurk@medienwerft.de>
2025-11-27 17:46:25 +0100

    SHOW-212 Fix translations for account > tasks

--- src/app/[site]/[locale]/(default)/account/tasks/page.tsx
+++ src/app/[site]/[locale]/(default)/account/tasks/page.tsx
@@ -4,5 +4,5 @@ import PlaceholderPage from '@/components/account/placeholder-page';
 export default function TasksPage() {
   const t = useTranslations('account.sidebar.items');
 
-  return <PlaceholderPage title={t('approvals')} />;
+  return <PlaceholderPage title={t('tasks')} />;
 }
