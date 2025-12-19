e106b44dabdef44d6b7c9c421886ef0f7da82225
David Gurk <dgurk@medienwerft.de>
2025-12-19 11:54:09 +0100

    SHOW-218 Prevent flickering of header cart tooltip on touch devices

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -61,7 +61,7 @@ function MiniCartTooltipContent({
         align="end"
         side="bottom"
         className={cn(
-          'hidden md:block backdrop-blur-default bg-surface-page/90 shadow-xl pl-4 pb-4 pt-0 pr-0 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) border-width-notification rounded-notification',
+          'hidden md:block md:[@media(pointer:coarse)]:hidden backdrop-blur-default bg-surface-page/90 shadow-xl pl-4 pb-4 pt-0 pr-0 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) border-width-notification rounded-notification',
           className,
         )}
         {...props}
