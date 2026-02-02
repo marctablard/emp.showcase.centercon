7517ca9a34dcf66ffc3200b6fa242fb53e56ed52
pbambynek <pawel.bambynek@tsh.io>
2026-02-02 19:47:31 +0100

    deleted unnecessary logs

--- src/platform/services/cart/impl/EmporixCartService.ts
+++ src/platform/services/cart/impl/EmporixCartService.ts
@@ -251,8 +251,7 @@ class EmporixCartService implements CartService {
     if (!site) {
       throw new Error('Site not found');
     }
-    console.log('site in updateCurrency: ', site);
-    console.log(' currency: ', currency);
+
     if (!site.currencies.find((siteCurrency) => siteCurrency.code === currency || siteCurrency.id === currency)) {
       throw new Error('Currency not supported');
     }
